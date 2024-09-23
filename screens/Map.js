6a0a72e98e1ac72724f7e643872915a8fce9c124
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-09-23 18:06:56 +0300

    Gpspaikannus toimintaan

--- screens/Map.js
+++ screens/Map.js
@@ -1,19 +1,57 @@
-import {View, Text, StyleSheet, SafeAreaView, Platform} from 'react-native'
-import React from 'react'
+import { StyleSheet, SafeAreaView, Platform } from 'react-native'
+import React, { useEffect } from 'react'
 import MapView from 'react-native-maps'
 import Constants from 'expo-constants'
+import { useState } from 'react'
+import * as Location from 'expo-location'
+
 
 
 export default function Map() {
+    const [location, setLocation] = useState({
+        /* Default location is University of Helsinki */
+        latitude: 60.1695,
+        longitude: 24.9354,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    })
+
+    useEffect(() => {
+        (async () => {
+            getUserPosition()
+        }
+        )()
+    }, [])
+
+    const getUserPosition = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync()
+
+        try {
+            if (status !== 'granted') {
+                console.log('Permission to access location was denied')
+                return
+            }
+            const position = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High })
+            setLocation({ ...location, "latitude": position.coords.latitude, "longitude": position.coords.longitude })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     return (
         <SafeAreaView style={styles.container}>
-        <MapView 
-            style={styles.map}
-            zoomControlEnabled={true}
+            <MapView
+                style={styles.map}
+                zoomControlEnabled={true}
+                region={location}
             />
         </SafeAreaView>
     )
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
@@ -24,8 +62,7 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
     },
     map: {
-     height: '100%',
+        height: '100%',
         width: '100%',
     },
-  });
-  
+});
